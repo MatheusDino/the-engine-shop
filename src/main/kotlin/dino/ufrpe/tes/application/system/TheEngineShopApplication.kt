bcd49ee72c4d913c65a25228375b55561141bd8d
Matheus Freitas <matheusthiago.dino@gmail.com>
2024-01-25 18:03:50 -0300

    subindo projeto do spring initializr

---
+++ src/main/kotlin/dino/ufrpe/tes/application/system/TheEngineShopApplication.kt
@@ -0,0 +1,11 @@
+package dino.ufrpe.tes.application.system
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TheEngineShopApplication
+
+fun main(args: Array<String>) {
+	runApplication<TheEngineShopApplication>(*args)
+}
---
+++ src/test/kotlin/dino/ufrpe/tes/application/system/TheEngineShopApplicationTests.kt
@@ -0,0 +1,13 @@
+package dino.ufrpe.tes.application.system
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TheEngineShopApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
