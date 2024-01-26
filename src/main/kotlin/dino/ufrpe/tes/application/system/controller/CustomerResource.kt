01b00282b4388475a66889c6eb38effcae231572
Matheus Freitas <matheusthiago.dino@gmail.com>
2024-01-26 20:03:13 -0300

    adicionado funcoes de visualizar e deletar

--- src/main/kotlin/dino/ufrpe/tes/application/system/controller/CustomerResource.kt
+++ src/main/kotlin/dino/ufrpe/tes/application/system/controller/CustomerResource.kt
@@ -1,7 +1,12 @@
 package dino.ufrpe.tes.application.system.controller
 
 import dino.ufrpe.tes.application.system.dto.CostumerDto
+import dino.ufrpe.tes.application.system.dto.CustomerView
+import dino.ufrpe.tes.application.system.entity.Customer
 import dino.ufrpe.tes.application.system.service.implementation.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,4 +23,13 @@ class CustomerResource(
        val savedCustomer = (this.customerService.saveCustomer(customerDto.toEntity()))
         return "Cliente ${savedCustomer.firstName} ${savedCustomer.lastName} cadastrado com sucesso!"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteCustomer(@PathVariable id: Long) = this.customerService.delete(id)
 }
