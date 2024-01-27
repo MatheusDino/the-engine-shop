ba82ab235e75595dc1beb9096387f2ffabd1143f
Matheus Freitas <matheusthiago.dino@gmail.com>
2024-01-27 13:46:35 -0300

    finalizado alguns testes e alterações

---
+++ db-scripts/admin-insert-into-db.sql
@@ -0,0 +1,2 @@
+INSERT INTO clientes (id, cpf, email, first_name, last_name, password, postal_code, street)
+VALUES (-1, '000000000', 'contato@theEngineShop.com', 'The Engine', 'Shop', 'muitoComplicado', '00123000', 'Avenida Velocidade');
